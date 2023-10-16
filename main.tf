06c7c49dc8642555d34baade39f44805915c4d55
wei <wei.zhong@shell.com>
2023-10-16 21:02:44 +0200

    add the required tags

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.policy.json
+  tags = {
+    env = "test"
+    owner = "wei"
+    department = "it"
+  }
 }
 resource "aws_lambda_function" "lambda" {
   function_name = "welcome"
@@ -9,4 +14,10 @@ resource "aws_lambda_function" "lambda" {
   role    = aws_iam_role.iam_for_lambda.arn
   handler = "welcome.lambda_handler"
   runtime = "python3.8"
+
+  tags = {
+    env = "test"
+    owner = "wei"
+    department = "it"
+  }
 }
